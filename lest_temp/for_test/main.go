863ffc7e3ac39a55ed0f5c0ac7c83f7b0479d3dc
BinScholl <BinScholl@foxmail.com>
2019-12-08 19:19:40 +0800

    updated new the source code 😃

--- lest_temp/for_test/main.go
+++ lest_temp/for_test/main.go
@@ -4,10 +4,16 @@ import "fmt"
 
 //计算一个字符串里面包含有多少个中文字符
 func main() {
-	var str string = "Hello世界!"
-	StrArray := []rune(str)
+	// var str string = "Hello世界!"
+	// StrArray := []rune(str)
+	// count := 0
+	// for _, c := range StrArray {
+	// 	if len(string(c)) == 3 {
+	// 		count++
+	// 	}
+	// }
 	count := 0
-	for _, c := range StrArray {
+	for _, c := range "Hello世界!" {
 		if len(string(c)) == 3 {
 			count++
 		}
