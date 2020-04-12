3b946191ba1b104cd694ce12efd41c7e345ea60e
Higker <deen.job@qq.com>
2020-04-12 13:14:15 +0800

    push bufio file reader and writer example source code for github

---
+++ lets_18_file/file_http_server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+//通过bufio读写文件
+const (
+	size = 1024
+	Path = "D:/Go_workspace/src/Lets_Go/lets_18_file/ks.txt"
+)
+
+//文件http服务器测试
+func main() {
+	http.HandleFunc("/ks", func(w http.ResponseWriter, r *http.Request) {
+		result := make(map[string]interface{})
+		result["data"] = outFile(w)
+		d, _ := json.Marshal(result)
+		fmt.Fprint(w, string(d))
+	})
+	http.HandleFunc("/video", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "video/mp4")
+		file, err := ioutil.ReadFile("C:/Users/Deen.job/Desktop/新建文件夹/out2.mp4")
+		if err != nil {
+			fmt.Fprintf(writer, "查无此视频")
+			return
+		}
+		writer.Write(file)
+	})
+	fmt.Println("服务器正常启动....")
+	http.ListenAndServe(":8080", nil)
+}
+
+func outFile(writer io.Writer) []string {
+	file, err := os.Open(Path)
+	defer file.Close() //关闭文件防止占用资源
+	if err != nil {
+		fmt.Fprintf(writer, "文件打开错误~~")
+	}
+	//通过bufio里面的方法读取文件
+	reader := bufio.NewReader(file)
+	content := make([]string, 0, size*8)
+
+	for {
+		temp, err := reader.ReadString('\n')
+		content = append(content, temp)
+		if err == io.EOF { //文件读取完毕
+			break
+		}
+	}
+	return content
+}
